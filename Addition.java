27f9b6083777662fe4f8c55ff51a428e57e5d119
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-09-13 10:45:48 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition {
+    public static void main (String [] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int Addition = (a+b+c);
+        System.out.println("Addition of 3 numbers is : "+Addition);
+    }
+}
---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division {
+    public static void main (String [] args) {
+        int a = 34;
+        int b = 8;
+        int c = a/b;
+        System.out.println("Result : "+c);
+    }
+} 
