f65aa0f0e82f5a5a028fbb7a7a860bf09720aae1
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-09-14 10:22:20 +0530

    Add files via upload

---
+++ Data_Types.java
@@ -0,0 +1,21 @@
+public class Data_Types {
+    public static void main (String [] args) {
+        int a = 2147483647;
+        byte b = 127;
+        short s = 32767;
+        long l = 9223372036854775807L;
+        float f = 8395.34F;
+        double d = 7327.909;
+        boolean boo = true;
+        char c = 'a';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(boo);
+        System.out.println(c);
+    }
+}
---
+++ Data_Types1.java
@@ -0,0 +1,6 @@
+public class Data_Types1 {
+    public static void main (String [] args) {
+        String arr = "Tejas Jagdale";
+        System.out.println(arr);
+    }
+}
