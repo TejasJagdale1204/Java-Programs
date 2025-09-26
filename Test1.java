9298788c66eb33b5712549d0fbcb4a2cd4ff9571
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-09-26 16:16:55 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,8 @@
+class Test1 {
+    public static void main (String [] args) {
+        String s = "Hello World";
+        for(int i=1;i<=6;i++) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,9 @@
+public class Test2 {
+    public static void main (String [] args) {
+        int i;
+        System.out.println("1st 10 natural numbers are : ");
+        for(i=1;i<11;i++) {
+            System.out.println(i);
+        }
+    }
+}
