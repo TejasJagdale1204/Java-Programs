64dde0afc7f2ec0ef3f4331085b32936ef322d7d
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-09-29 01:15:43 +0530

    Add files via upload

---
+++ Inheritance1.java
@@ -0,0 +1,8 @@
+class Inheritance1 {
+    Inheritance1() {
+        System.out.println("Parent class Inheritance1 constructor is executed"
+        +"\nfor initializing class Inheritance1 NSV's in this object "+this);
+        
+        System.out.println();
+    }
+}
---
+++ Inheritance2.java
@@ -0,0 +1,6 @@
+class Inheritance2 extends Inheritance1{
+    Inheritance2() {
+        System.out.println("Child class Inheritance2 constructor is executed"
+        +"\nfor initializing class Inheritance2 NSV's in this object "+  this);
+    }
+}
