ea9b560e104160166bfed018c5c3e16545dfcd9f
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-02 22:06:12 +0530

    Add files via upload

---
+++ unary_minus.java
@@ -0,0 +1,16 @@
+public class unary_minus {
+    public static void main(String[] args) {
+        // Declaring a custom variable
+        int n1 = 20;
+
+        // Printing the above variable
+        System.out.println("Number = " + n1);
+
+        // Performing unary operation
+        n1 = -n1;
+
+        // Printing the above result number
+        // after unary operation
+        System.out.println("Result = " + n1);
+    }
+}
