aa15ebce20f7adc25b0a9f3ecd802c7ec95c07a8
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-03 18:18:51 +0530

    Add files via upload

---
+++ MH_MOVR_MOVL1.java
@@ -0,0 +1,10 @@
+class MH_MOVR_MOVL1 {
+    static int a = 10;
+    int x = 20;
+    static void m1() {
+        System.out.println("A m1");
+    }
+    void m2 () {
+        System.out.println("A m2");
+    }
+}
---
+++ MH_MOVR_MOVL2.java
@@ -0,0 +1,14 @@
+class MH_MOVR_MOVL2 extends MH_MOVR_MOVL1 {
+    static int a = 50;
+    int x = 60;
+
+    static void m1() {
+        System.out.println("B m1");
+    }
+    void m2() {
+        System.out.println("B m2");
+    }
+    void m3() {
+        System.out.println("B m3");
+    }
+}
