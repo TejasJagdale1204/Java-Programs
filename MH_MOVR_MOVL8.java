46ada478d19d4ac01046891862972f5970f7e00d
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-05 23:10:52 +0530

    Add files via upload

---
+++ MH_MOVR_MOVL8.java
@@ -0,0 +1,8 @@
+class MH_MOVR_MOVL8 extends MH_MOVR_MOVL7 {
+    public static void main (String [] args) {
+        MH_MOVR_MOVL7.add(5,6);
+        MH_MOVR_MOVL7.add(45.18f,93.07f);
+        MH_MOVR_MOVL7.add("Tejas ","Prafull");
+        
+    }
+}
