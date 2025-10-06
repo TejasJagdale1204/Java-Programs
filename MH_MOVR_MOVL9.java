ed3549eb6ea3e36a1ed3ffa72e7084670b01fedb
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-06 10:26:00 +0530

    Add files via upload

---
+++ MH_MOVR_MOVL9.java
@@ -0,0 +1,11 @@
+class MH_MOVR_MOVL9 {
+    static void m1(int x) {
+        System.out.println("int-param");
+    }
+    static void m1(float f) {
+        System.out.println("float-param");
+    }
+    static void m1(String s) {
+        System.out.println("String-param");
+    }
+}
