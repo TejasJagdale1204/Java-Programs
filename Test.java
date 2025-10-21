47b048b2cbfd7353e414e8495ed23982496d9454
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-21 22:30:36 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+// Test.java
+package Example1;
+public class Test {
+    public static void main (String [] args) {
+        Sample.add(7,6);
+    }
+}
+
+//Run in Terminal in this way
+//javac -d . Sample.java
+//javac -d . Test.java
+//java Test
+//Then we will get output
