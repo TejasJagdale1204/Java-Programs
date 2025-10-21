71077eeae38a73b8b6d23aa9ec180736826281fd
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-21 22:31:19 +0530

    Add files via upload

--- Addition.java
+++ Addition.java
@@ -1,10 +1,7 @@
-class Addition {
-    public static void main (String [] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-
-        int Addition = (a+b+c);
-        System.out.println("Addition of 3 numbers is : "+Addition);
+//Addition.java
+package Tejas;
+public class Addition {
+    public static void add(int a , int b) {
+        System.out.println("Result : "+(a+b));
     }
 }
