af4b16dd5e28bb62f2ced045fa29fce101ed00fd
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-10-29 21:36:32 +0530

    Add files via upload

---
+++ Searching_Operations1.java
@@ -0,0 +1,11 @@
+public class Searching_Operations1 {
+    public static void main(String[] args) {
+        String s1 = "java hari krishna";
+
+        System.out.println(s1.contains("hari"));
+        System.out.println(s1.contains("krish"));
+        System.out.println(s1.contains("avaj"));
+        System.out.println(s1.contains("Java"));
+        System.out.println(s1.contains("hari kri"));
+    }
+}
