fee5a1d08e1c95351882ea6a698baf31f0a4f3af
TejasJagdale1204 <tejasjagdale45@gmail.com>
2025-11-01 20:16:31 +0530

    Add files via upload

---
+++ Converting_Operations1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Converting_Operations1 {
+    public static void main(String[] args) {
+        
+        String s1 = "Shivtej";
+        System.out.println(s1.toCharArray());
+
+        String s2 = "Tejas";
+        char[] ca = s2.toCharArray();
+        System.out.println(Arrays.toString(ca));
+    }
+}
